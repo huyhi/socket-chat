d9fb4c6e829cdd9e4f87d9689b61ce6b84798caa
annhuny <annhuny@annhunys-MacBook-Pro.local>
2019-01-28 22:30:57 +0800

    fix

--- src/main/java/client/SingleClient.java
+++ src/main/java/client/SingleClient.java
@@ -1,43 +1,25 @@
 package client;
 
-import asyncJob.ServerListenThread;
+import terminal.dialogTerminal;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class SingleClient {
     private String host;
     private int port;
     private Socket socket;
-    private OutputStream outputStream;
 
     public SingleClient(String host, int port) {
         this.host = host;
         this.port = port;
     }
 
-    public void sendMsg(String msg) throws IOException {
-        try {
-            outputStream.write(msg.getBytes("UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-    }
-
     public void connectToServer() throws IOException {
         this.socket = new Socket(host, port);
         System.out.println("success connect to " + host + ":" + port);
-
-        new Thread(new ServerListenThread(socket)).start();
-
-        this.outputStream = socket.getOutputStream();
-        Scanner stdInReader = new Scanner(System.in);
-        while (stdInReader.hasNext()) {
-            sendMsg(stdInReader.next());
-        }
+        //开始聊天框
+        new dialogTerminal(socket).open();
     }
 
 
--- src/main/java/server/SingleServer.java
+++ src/main/java/server/SingleServer.java
@@ -1,31 +1,20 @@
 package server;
 
-import asyncJob.ServerListenThread;
+import terminal.dialogTerminal;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class SingleServer {
     public int port;
     public ServerSocket serverSocket;
     public Socket socket;
-    public OutputStream outputStream;
 
     public SingleServer(int port) {
         this.port = port;
     }
 
-    public void sendMsg(String msg) throws IOException {
-        try {
-            outputStream.write(msg.getBytes("UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-    }
 
     public void runServer() throws IOException {
         this.serverSocket = new ServerSocket(port);
@@ -36,14 +25,8 @@ public class SingleServer {
             //thread will be blocked until a request arrive
             System.out.println("accept request from " + socket.getInetAddress().getHostAddress());
 
-            //start the thread to listen client server and print out
-            new Thread(new ServerListenThread(socket)).start();
-
-            this.outputStream = socket.getOutputStream();
-            Scanner stdInReader = new Scanner(System.in);
-            while (stdInReader.hasNext()) {
-                sendMsg(stdInReader.next());
-            }
+            //开始聊天框
+            new dialogTerminal(socket).open();
         }
     }
 
